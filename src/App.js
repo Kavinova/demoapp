d2900e3609ea8c19a3083ce414cdcb4b341c9df2
rajnish <rajnishraj4@gmail.com>
2020-06-27 14:30:34 +0530

    added base project structure

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+import './App.css';
+
+class App extends Component {
+    render(){
+      return(
+        <div className="App1">
+        <h1>Lets start react js</h1>
+        </div>
+      );
+    }
+}
+
+export default App;
---
+++ src/person/Person.js
@@ -0,0 +1,6 @@
+import React,{Component} from 'react';
+import './Person.css'
+
+class Person extends Component{
+  
+}
